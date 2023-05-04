b9f802cda08b869e7a87261f6daf7a08a5dd6045
mikeltim257 <tn.mikeladze@tensor.ru>
2023-05-04 15:06:33 +0700

    haha

--- Lesson2_timuri/lesson2_case7_timuri.py
+++
@@ -1,4 +0,0 @@
-string1 = 'wtf'
-string2 = 'mon uybbjo cxgujjs whjk tcx afdp'
-
-print(string2[(string2.find(min(string1[0]))):(string2.find(max(string1[2]))) + 1:])
--- Lesson3_timuri/spiski.py
+++
@@ -1,9 +0,0 @@
-a = [3,'hello',[1,2,3],5.7]
-b = list('String')
-
-print(a, type(a))
-print(b, type(b))
-
-
-print('hello' in a)
-
