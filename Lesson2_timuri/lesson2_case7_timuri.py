e6709789ca19c2cb7c97934c479546b329771868
mikeltim257 <tn.mikeladze@tensor.ru>
2023-05-04 14:54:53 +0700

    vosstanovil

--- Lesson2_timuri/lesson2_case7_timuri.py
+++
@@ -1,8 +0,0 @@
-string1 = 'wtf'
-string2 = 'mon uybbjo cxgujjs whjk tcx afdp'
-
-a = string2.find(string1[0])
-b = string2.find(string1[1])
-c = string2.find(string1[2])
-
-print(string2[min(a, b, c):max(a, b, c) + 1])
